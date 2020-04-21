455e033a9ee2b04dc9bf6bd42497621173fca3d0
PireIre <bozoire@gmail.com>
2020-04-21 09:54:40 +0200

    -Used different Homepage Content. -Fixed Navbar -Changed Tab Icon and Title

--- frontend/src/components/chat/ChatPage.js
+++ frontend/src/components/chat/ChatPage.js
@@ -5,7 +5,7 @@ class ChatPage extends Component {
     render() {
         return (
             <div>
-
+                <h1>Feature still in development....Thank you for your patience</h1>
             </div>
         );
     }
--- frontend/src/components/home/HomePage.js
+++ frontend/src/components/home/HomePage.js
@@ -6,17 +6,15 @@ class HomePage extends Component {
             <div className="card">
                 <div className="card-body">
                     <h4 className="card-title">Quaranchat for different Nordic Countries</h4>
-                    <p>Quranchat is an online forum to help people communicate effectively
-                        during life changing events such as quarantines, recessions and
-                        unprecedented events that make people stay home and wear out.
-                        We allow our users to create, delete, view posts and comments.
-                        Here we encourage users to share ideas, thoughts, inspirations to kill time and fight boredom.</p>
+                    <p>Quranchat is an online forum to help people communicate effectively during life changing events such as quarantines, recessions and unprecedented events that make people stay home and wear out. We allow our users to create, delete, view posts and comments. Here we encourage users to share ideas, thoughts, inspirations to kill time and fight boredom.</p>
+                    <p>Take a leaf from our inspirations in creating this forum:</p>
                     <ul>
-                        <li><a href="https://spring.io/projects/spring-boot">Sweden</a></li>
-                        <li><a href="https://www.postgresql.org">Finland</a></li>
-                        <li><a href="https://reactjs.org">Norway</a></li>
-                        <li><a href="https://reacttraining.com/react-router/web/guides/quick-start">Denmark</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Quarantine">Quarantine</a></li>
+                        <li><a href="https://en.wikipedia.org/wiki/Online_chat">Chat</a></li>
+                        <li><a href="https://www.awesomeinventions.com/silly-things/">Kill Time</a></li>
+                        <li><a href="https://www.boredpanda.com/"> Bored Panda</a></li>
                     </ul>
+
                 </div>
             </div>
         );
